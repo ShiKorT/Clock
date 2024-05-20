40b628fc4787e68938278c17e2d4f007263db179
Artem <supertema38@gmail.com>
2024-05-20 20:06:40 +0300

    изминил формат фремени и добавил дату

--- Clock.py
+++ Clock.py
@@ -2,11 +2,18 @@ import tkinter
 import time
 import sys
 import os
+from datetime import date
+
+def data():
+    data_show = date.today()
+    txt5.config(text=data_show)
+
 
 def timing():
-    time_cur = time.strftime("%H : %M : %S")
-    txt2.config(text=time_cur)
-    txt2.after(200, timing)
+    time_cur = time.strftime("%I : %M : %S")
+    txt3.config(text=time_cur)
+    txt3.after(200, timing)
+
 def restart():
     res = sys.executable
     os.execl(res, res, *sys.argv)
@@ -15,22 +22,29 @@ win = tkinter.Tk()
 WID = (win.winfo_screenwidth() // 2) - 300
 HEI = (win.winfo_screenheight() // 2) - 150
 win.geometry(f"600x300+{WID}+{HEI}")
-win.title("Часы")
+win.title("Дата и время")
 win.resizable(False, False)
 
 
-txt1 = tkinter.Label(win, text="Часы", font=("Arial Bold", 32))
+txt1 = tkinter.Label(win, text="Дата и время", font=("Arial Bold", 32))
 txt1.pack()
 
-txt2 = tkinter.Label(win, font=("Arial Bold", 60), background="green")
-txt2.place(x=100,y=80)
+txt2 = tkinter.Label(win, text="(12-часовой)", font=("Arial Bold", 12))
+txt2.place(x=260, y=50)
+
+txt3 = tkinter.Label(win, font=("Arial Bold", 60), background="green")
+txt3.place(x=100, y=117)
 timing()
 
-txt3 = tkinter.Label(win, text="hors" + " "*13+"minuts"+ " "*10+ "seconds",  font=("Arial Bold", 18))
-txt3.place(x=125,y=200)
+txt4 = tkinter.Label(win, text="hors" + " "*13+"minuts"+ " "*10+ "seconds",  font=("Arial Bold", 18))
+txt4.place(x=125, y=227)
+
+txt5 = tkinter.Label(win, font=("Arial Bold", 18))
+txt5.place(x=250, y=77)
+data()
 
 btn_res = tkinter.Button(text="Перезагрузить", command=restart)
-btn_res.place(x=450,y=270)
+btn_res.place(x=450, y=270)
 
 
 btn_exit = tkinter.Button(win, text="Выйти", command=sys.exit)
